02f7b3d0fa9774ebdd901a9d24cd806e16aada96
Aaron Asch <aasch@athenahealth.com>
2017-11-05 16:31:41 +0000

    Deply Me Please!

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -16,3 +16,22 @@ app.controller("batchRequest", function($scope, $http) {
         });
     }
 });
+
+app.controller("byPayer", function($scope, $http) {
+  $scope.submit = function (cpt) {
+    var path = '/byPayer?' + 'cpt=' + cpt;
+    $http.get(path).then(function (response) {
+      var rows = [];
+      _.forEach(_.keys(response.data), function(insurance) {
+        var thisInsuranceClaimCount = response.data[insurance][cpt];
+        if(thisInsuranceClaimCount) {
+          rows.push({
+            insurance: insurance,
+            thisInsuranceClaimCount: thisInsuranceClaimCount
+          });
+        }
+      });
+      $scope.results = rows;
+    });
+  }
+});
